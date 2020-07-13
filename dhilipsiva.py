17a502014e7fb1b288befe8b5be6c5064e382c5a
dhilipsiva <dhilipsiva@pm.me>
2020-07-13 11:13:41 +0530

    Add dhilipsiva.py

---
+++ dhilipsiva.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from spacetime import SpaceTime
+
+
+class Dhilipsiva(SpaceTime):
+
+    def __init__(self):
+        self.name = 'dhilipsiva'
+
+    def labels(self):
+        return ['Optimistic Nihilist', 'Democratic Socialist 🌹', 'N00b Astrophysicist']  # noqa: E501
+
+    def beliefs(self):
+        return ['Everybody is equal', 'An instance of spacetime, just like you']  # noqa: E501
+
+    def lover_of(self):
+        return ['Science', 'Python', 'FOSS', 'தமிழ்']
+
+    def roles(self):
+        return ['VP of Engineering @reckonsys', 'Care Taker @nitimis']
+
+    def probable_activities(self):
+        return ['Coding (Terminal/Vim/GitHub)', 'Teaching Kids', 'Gaming', 'Sleeping']  # noqa: E501
